f99e5b452655a8d17475ad3261080d2865b3a2b5
gongzitao <gongzier@gmail.com>
2014-11-22 10:43:45 -0600

    generate parentheses
    combination problems

---
+++ generate parenthese.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class Solution {
+    public ArrayList<String> generateParenthesis(int n) {
+        ArrayList<String> ret = new ArrayList<String>();
+        if(n == 0) 
+            return ret;
+        helper(n,n,new StringBuilder(), ret);
+        return ret;
+    }
+    private void helper(int left,int right, StringBuilder str, ArrayList<String> ret){
+        if(left == 0 && right == 0) {
+            ret.add(new String(str.toString()));
+            return;
+        }
+        if(left < 0 || right < 0) return;
+        if(left <= right){
+            str.append('(');
+            helper(left - 1, right, str,ret);
+        }
+        str.append(')');
+        helper(left, right - 1,str, ret);
+    }
+}
