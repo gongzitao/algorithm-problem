c6daabce0c4e2c5858f05dc884fda90f122ce535
gongzitao <gongzier@gmail.com>
2015-01-06 11:50:54 -0800

    FindPeakElement

--- 3sum.java
+++ 3sum.java
@@ -1,42 +1,23 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 public class Solution {
-    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
-        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
-        if(num == null || num.length < 3) return ret;
-        Arrays.sort(num);
-        for(int i = 0;i < num.length - 2; i++){
-            //for(int j = i + 1; j < num.length; j++){
-            int j = i + 1;
-            int k = num.length - 1;
-            if(i == 0 || num[i] != num[i - 1]){
-                while(j < k){
-                    
-                    if(num[j] + num[k] == -num[i]){
-                        ArrayList<Integer> item = new ArrayList<Integer>();
-                        item.add(num[i]);
-                        item.add(num[j]);
-                        item.add(num[k]);
-                        ret.add(item);
-                        j++;
-                        k--;
-                        // remove duplicate
-                        while(j < k && num[j] == num[j - 1]){
-                            j++;
-                        }
-                        while(k > j && num[k] == num[k + 1]){
-                            k--;
-                        }
-                    }
-                    else if(num[j] + num[k] < -num[i]){
-                        j++;
-                    }
-                    else{
-                        k--;
-                    }
-                }
-            } 
-        }
+    public ArrayList<String> generateParenthesis(int n) {
+        ArrayList<String> ret = new ArrayList<String>();
+        if(n == 0)
+            return ret;
+        helper(n,n,new StringBuilder(), ret);
         return ret;
+    }
+    private void helper(int left,int right, StringBuilder str, ArrayList<String> ret){
+        if(left == 0 && right == 0) {
+            ret.add(new String(str.toString()));
+            return;
+        }
+        if(left < 0 || right < 0) return;
+        if(left <= right){
+            str.append('(');
+            helper(left - 1, right, str,ret);
+        }
+        str.append(')');
+        helper(left, right - 1,str, ret);
     }
 }
--- sortcolor.java
+++ sortcolor.java
@@ -1,24 +1,36 @@
 public class Solution {
-    public void sortColors(int[] A) {
-        if(A == null || A.length == 0) return;
-        int i = 0;
-        int red = 0;
-        int blue = A.length - 1;
-        while(i <= blue){
-            if(A[i] == 0){
-                int temp = A[i];
-                A[i] = A[red];
-                A[red] = temp;
-                red++;
-                i++;
+    public int findPeakElement(int[] num) {
+        if(num == null || num.length == 0)
+            return 0;
+        int left = 0;
+        int right = num.length - 1;
+        while(left < right){
+            int mid = (left + right) / 2;
+            if(mid == num.length - 1)
+                if(num[mid] > num[mid - 1])
+                    return mid;
+                else return mid - 1;
+            if(mid == 0)
+                if(num[mid] > num[mid + 1])
+                    return mid;
+                else return mid + 1;
+            if(num[mid] > num[mid - 1] && num[mid] > num[mid + 1])
+                return mid;
+            if(num[mid] > num[mid - 1] && num[mid] < num[mid + 1]){
+                left = mid + 1;
             }
-            else if(A[i] == 2){
-                int temp = A[i];
-                A[i] = A[blue];
-                A[blue] = temp;
-                blue--;
+            //else right = mid - 1;
+            if(num[mid] > num[mid + 1] && num[mid] < num[mid - 1]){
+                right = mid - 1 ;
             }
-            else i++;
+            //else right = mid + 1;
+            if(num[mid] < num[mid - 1] && num[mid] < num[mid + 1]){
+                if(num[mid - 1] > num[mid + 1])
+                    right = mid - 1;
+                else left = mid + 1;
+            }
+            
         }
+        return left;
     }
 }
